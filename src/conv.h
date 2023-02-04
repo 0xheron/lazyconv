30a37e12cf30f132780059a9c97a643cd6c1d5e4
oxheron <96091489+0xheron@users.noreply.github.com>
2023-02-04 22:36:18 +0000

    Conv function

---
+++ src/conv.h
@@ -0,0 +1,33 @@
+#pragma once
+
+template <uint32_t c, uint32_t k>
+std::array<std::array<uint32_t, c - k + 1>, c - k + 1> convolute2D(const std::array<std::array<uint32_t, c>, c>& input, const std::array<std::array<uint32_t, k>, k>& kernel)
+{
+    std::array<std::array<uint32_t, c - k + 1>, c - k + 1> rval;
+    for (size_t i = 0; i < rval.size(); i++)
+    {
+        for (size_t j = 0; i < rval.size(); i++)
+        {
+            // Get center or corner square 
+            // If corner, iterate over other squares (its upper left corner), multiply by index in 
+            // If center, iterate over other squares
+
+            size_t convolute = 0;
+
+            for (size_t x = 0; x < k; x++)
+            {
+                for (size_t y = 0; y < k; y++)
+                {
+                    convolute += input[i + x][j + y] * kernel[x][y];
+                }
+            }
+
+            rval[i][j] = convolute;
+        }
+    }
+
+    return rval;
+}
+
+template <uint32_t c, uint32_t k> 
+std::array<std::array<uint32_t, c + k - 1>, c + k - 1> deconvolute2D
